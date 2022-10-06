f8b69ba6c6dae99c5d47a87045307607e8b4a78f
ImBioMario <66173536+ImBioMario@users.noreply.github.com>
2022-10-06 14:23:23 +0200

    implemented time

--- lab1/source/server.py
+++ lab1/source/server.py
@@ -2,6 +2,8 @@
 import http.server
 import socketserver
 import os
+import datetime
+from threading import local
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -10,13 +12,16 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
-        
+        local_time = datetime.datetime.now()
+        local_time_string = str(local_time.strftime('%X'))
+        message = 'Hello world! \n' + local_time_string
+        print(local_time_string)
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.wfile.write(bytes(message.encode('utf-8')))
         else:
             super().do_GET()
     
