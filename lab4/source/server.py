6ce0d77e6edea9a630098277e2417733fd7f9be2
ImBioMario <m.sukiennik@gmail.com>
2022-11-17 12:53:20 +0100

    trying if works

--- lab4/source/server.py
+++ lab4/source/server.py
@@ -5,12 +5,13 @@ parameter_names = ["sum", "sub", "mul", "div", "mod"]
 
 server = Flask()
 
-def get_operation_
+def get_operation_stats(num1 :int, num2: int):
+    return [num1+num2, num1-num2, num1*num2, num1//num2, num1%num2]
 
 
 @app.get('/')
 def generate_stats():
     num1 = request.args.get('num1')
     num2 = request.args.get('num2')
-
-
+    my_json = jsonify(dict(zip(parameter_names, get_operation_stats(num1, num2))))
+    return my_json
