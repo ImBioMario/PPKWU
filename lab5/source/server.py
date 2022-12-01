bc844a6f38b5133e92c0d9d8c4645d9ceca9f05e
ImBioMario <m.sukiennik95@gmail.com>
2022-12-01 14:36:04 +0100

    attempting post

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -9,11 +9,12 @@ def get_operation_stats(num1 :int, num2: int):
     return [num1+num2, num1-num2, num1*num2, num1//num2, num1%num2]
 
 
-@app.get('/')
+@app.post('/')
 def generate_stats():
-    num1 = request.args.get('num1',type = int)
-    num2 = request.args.get('num2', type = int)
-    my_json = jsonify(dict(zip(parameter_names, get_operation_stats(num1, num2))))
-    return my_json
+    data = request.get_json
+
+
+
+    return data
 
 app.run(port=4080, host='0.0.0.0')
